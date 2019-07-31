1abe04eb4108fdc3c8b60e29103567e925304819
NekuSoul <Git@NekuSoul.de>
2019-07-31 22:42:06 +0200

    Added custom keymap

---
+++ keyboards/dztech/dz65rgb/keymaps/nekusoul/keymap.c
@@ -0,0 +1,61 @@
+#include QMK_KEYBOARD_H
+#define _LAYER0 0
+#define _LAYER1 1
+#define _LAYER2 2
+#define _LAYER3 3
+#define _LAYER4 4
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+        [_LAYER0] = LAYOUT_65_ansi( /* Base */
+            KC_GESC,  KC_1,     KC_2,     KC_3,     KC_4,   KC_5,   KC_6,   KC_7,   KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,    KC_BSPC,    KC_DEL,\
+            KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLASH,  KC_PGUP,\
+            MO(1),    KC_A,     KC_S,     KC_D,     KC_F,   KC_G,   KC_H,   KC_J,   KC_K,     KC_L,    KC_SCLN,  KC_QUOT,             KC_ENT,     KC_PGDN,\
+            KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,            KC_RSFT,   KC_UP,      KC_END,\
+            KC_LCTL,  KC_LGUI,  KC_LALT,                    KC_SPC,                 MO(2),    MO(3),   MO(4),              KC_LEFT,   KC_DOWN,    KC_RIGHT),
+        [_LAYER1] = LAYOUT_65_ansi( /* Function keys */
+            KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,    KC_NO,      KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,     KC_NO,      KC_NO,\
+            KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,    KC_NO,               KC_NO,      KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,              KC_NO,     KC_NO,      KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,                      KC_NO,                  KC_NO,    KC_NO,   KC_NO,              KC_NO,     KC_NO,      KC_NO),
+        [_LAYER2] = LAYOUT_65_ansi( /* Keyboard control */
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,     KC_NO,      KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,     KC_NO,      KC_NO,\
+            KC_NO,    RGB_VAI,  RGB_HUI,  RGB_SAI,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,    KC_NO,               KC_NO,      KC_NO,\
+            KC_NO,    RGB_VAD,  RGB_HUD,  RGB_SAD,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,              RGB_TOG,   RGB_SPI,    KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,                      KC_NO,                  KC_TRNS,  KC_NO,   KC_NO,              RGB_RMOD,  RGB_SPD,    RGB_MOD),
+        [_LAYER3] = LAYOUT_65_ansi( /* Unused */
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,     KC_NO,      KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,     KC_NO,      KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,    KC_NO,               KC_NO,      KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,              KC_NO,     KC_NO,      KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,                      KC_NO,                  KC_NO,    KC_TRNS, KC_NO,              KC_NO,     KC_NO,      KC_NO),
+        [_LAYER4] = LAYOUT_65_ansi( /* Unused */
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,     KC_NO,      KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,     KC_NO,      KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,    KC_NO,               KC_NO,      KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,              KC_NO,     KC_NO,      KC_NO,\
+            KC_NO,    KC_NO,    KC_NO,                      KC_NO,                  KC_NO,    KC_NO,   KC_TRNS,            KC_NO,     KC_NO,      KC_NO),
+};
+
+void rgb_matrix_indicators_user(void)
+{
+        if (IS_LED_ON(host_keyboard_leds(), USB_LED_CAPS_LOCK))
+        {
+          rgb_matrix_set_color(8, 0xFF, 0xFF, 0xFF);
+        }
+}
+
+void matrix_init_user(void)
+{
+  //user initialization
+}
+
+void matrix_scan_user(void)
+{
+  //user matrix
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+      return true;
+}

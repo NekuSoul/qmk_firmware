3e516bbefa67fbfc86df6e369dcd45bb2eb0162a
NekuSoul <Git@NekuSoul.de>
2019-08-25 14:14:36 +0200

    Added missing F11 button

--- keyboards/dztech/dz65rgb/keymaps/nekusoul/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/nekusoul/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z,   KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_HOME, KC_UP,   KC_END,\
         KC_LCTL, KC_LGUI, KC_LALT,                    KC_SPC,                    TT(2),   TT(4),   TT(3),            KC_LEFT, KC_DOWN, KC_RGHT),
     [_LAYER1] = LAYOUT_65_ansi( /* Function keys */
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,  KC_F10,  _______,  KC_F12, _______, KC_PSCR,\
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,  KC_F10,  KC_F11,  KC_F12, _______, KC_PSCR,\
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,\
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,\

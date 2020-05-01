ae777f49607dda00aa936546f1054c2af7400f72
NekuSoul <git@nekusoul.de>
2020-05-01 12:46:04 +0200

    Renamed layers

--- keyboards/dztech/dz65rgb/keymaps/nekusoul/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/nekusoul/keymap.c
@@ -1,36 +1,36 @@
 #include QMK_KEYBOARD_H
-#define _LAYER0 0
-#define _LAYER1 1
-#define _LAYER2 2
-#define _LAYER3 3
-#define _LAYER4 4
+#define _LAYERDEF 0
+#define _LAYERFNK 1
+#define _LAYERKBC 2
+#define _LAYERVNP 3
+#define _LAYERRGB 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_LAYER0] = LAYOUT_65_ansi( /* Base */
+    [_LAYERDEF] = LAYOUT_65_ansi( /* Default */
         KC_GESC, KC_1,   KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC, KC_PGUP,\
         KC_TAB,  KC_Q,   KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS, KC_PGDN,\
         TT(1),   KC_A,   KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,  KC_HOME,\
         KC_LSFT, KC_Z,   KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_DEL,  KC_UP,   KC_END,\
         KC_LCTL, KC_LGUI, KC_LALT,                    KC_SPC,                    TT(2),   TT(4),   TT(3),            KC_LEFT, KC_DOWN, KC_RGHT),
-    [_LAYER1] = LAYOUT_65_ansi( /* Function keys */
+    [_LAYERFNK] = LAYOUT_65_ansi( /* Function keys */
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,  KC_F10,  KC_F11,  KC_F12, _______, KC_PSCR,\
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,\
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,\
         _______, _______, _______,                   _______,                   XXXXXXX, XXXXXXX, XXXXXXX,          _______, _______, _______),
-    [_LAYER2] = LAYOUT_65_ansi( /* Keyboard control */
+    [_LAYERKBC] = LAYOUT_65_ansi( /* Keyboard control */
         RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
         XXXXXXX, RGB_VAI, RGB_HUI, RGB_SAI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,\
         XXXXXXX, RGB_VAD, RGB_HUD, RGB_SAD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          RGB_TOG, RGB_SPI, XXXXXXX,\
         XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,                   _______, XXXXXXX, XXXXXXX,          RGB_RMOD,RGB_SPD, RGB_MOD),
-    [_LAYER3] = LAYOUT_65_ansi( /* Virtual Numpad */
+    [_LAYERVNP] = LAYOUT_65_ansi( /* Virtual Numpad */
         _______, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______, _______,  _______, _______,\
         _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,  _______, _______,\
         XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,           _______, _______,\
         _______, KC_PDOT, KC_P0,   KC_PENT, KC_PPLS, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______,          _______,  _______, _______,\
         _______, _______, _______,                   XXXXXXX,                   XXXXXXX, XXXXXXX, _______,          _______,  _______, _______),
-    [_LAYER4] = LAYOUT_65_ansi( /* Indicator customization */
+    [_LAYERRGB] = LAYOUT_65_ansi( /* RGB customization */
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,\
@@ -48,7 +48,7 @@ uint8_t color_state = 1;
 void rgb_matrix_indicators_user(void)
 {
     switch (biton32(layer_state)) {
-        case _LAYER0:
+        case _LAYERDEF: /* Default */
             for(uint8_t row = 0; row < MATRIX_ROWS; row++)
             {
                 for(uint8_t col = 0; col < MATRIX_COLS; col++)
@@ -67,28 +67,28 @@ void rgb_matrix_indicators_user(void)
                 }
             }
             break;
-        case _LAYER1:
+        case _LAYERFNK: /* Function keys */
             rgb_matrix_set_color(30, 0xFF, 0x00, 0x00);
             for(uint8_t i = 0; i < 13; i++)
             {
                 rgb_matrix_set_color(functionkeys[i], 0xFF, 0x00, 0x00);
             }
             break;
-        case _LAYER2:
+        case _LAYERKBC: /* Keyboard control */
             rgb_matrix_set_color(62, 0xFF, 0x00, 0x00);
             for(uint8_t i = 0; i < 12; i++)
             {
                 rgb_matrix_set_color(controlkeys[i], 0xFF, 0x00, 0x00);
             }
             break;
-        case _LAYER3:
+        case _LAYERVNP: /* Virtual Numpad */
             rgb_matrix_set_color(64, 0xFF, 0x00, 0x00);
             for(uint8_t i = 0; i < 16; i++)
             {
                 rgb_matrix_set_color(numkeys[i], 0xFF, 0x00, 0x00);
             }
             break;
-        case _LAYER4:
+        case _LAYERRGB: /* RGB customization */
             for(uint8_t row = 0; row < MATRIX_ROWS; row++)
             {
                 for(uint8_t col = 0; col < MATRIX_COLS; col++)
@@ -135,7 +135,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 
     switch (biton32(layer_state)) {
         // Handle custom indicator layer
-        case _LAYER4:
+        case _LAYERRGB: /* RGB customization */
             if(keycode==TT(4))
             {
                 return true;

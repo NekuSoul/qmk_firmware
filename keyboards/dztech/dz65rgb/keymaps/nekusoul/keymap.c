0622106d1c34465ba51ac07665ce1c621ef1b1cd
NekuSoul <git@nekusoul.de>
2020-05-18 00:12:22 +0200

    Fix spacing in file

--- keyboards/dztech/dz65rgb/keymaps/nekusoul/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/nekusoul/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,   KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS, KC_PGDN,\
         TT(1),   KC_A,   KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,  KC_HOME,\
         KC_LSFT, KC_Z,   KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_DEL,  KC_UP,   KC_END,\
-        KC_LCTL, KC_LGUI, KC_LALT,                    KC_SPC,                    TT(2),   TT(4),   TT(3),            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                    TT(2),   TT(4),   TT(3),             KC_LEFT, KC_DOWN, KC_RGHT),
     [_LAYERFNK] = LAYOUT_65_ansi( /* Function keys */
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,  KC_F10,  KC_F11,  KC_F12, _______, KC_PSCR,\
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
@@ -159,7 +159,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
                 // Disable color
                 rgb_indicator_state[record->event.key.row][record->event.key.col] = 0;
             }
-            return true;
+            return true; 
     }
 
     return true;
